4b243f7c1c41a90d04149f384f2ce153c329f0d1
Tom Thomas <Tom Thomas@192.168.2.9>
2017-03-08 01:00:43 -0500

    Added dependencies for Grizzly Websocket library and as4j.

--- src/MasterServer.java
+++ src/MasterServer.java
@@ -1,3 +1,12 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MasterServer {
+	private static int PORT = 8080;
+	private static Logger log;
 	
+	public static void main(String[] args) {
+		log = Logger.getLogger(MasterServer.class.toString());
+		log.log(Level.INFO, "Starting up Master Server");
+	}
 }
